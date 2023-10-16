e2a1785f793f255dad03945244f3e7a9ae965f09
JavierHernandezMartinezUDC <javier.hernandez.martinez@udc.es>
2023-10-16 09:41:24 +0200

    16/10: arreglado problema create

--- p1.c
+++ p1.c
@@ -13,6 +13,7 @@ int TrocearCadena(char* cadena, char* trozos[]){
     return i;
 }
 
+
 void printRoute(){
     char path[1024];
 
@@ -25,22 +26,25 @@ void printRoute(){
 }
 
 void create(char *trozos[]){
+    int df;
+
     if(trozos[1]==NULL){
-        //Imprime ruta
         printRoute();
     }else if(strcmp(trozos[1],"-f")==0){
         if(trozos[2]==NULL){
             printRoute();
         }
         else{
-            //Crear fichero
-            if(open(trozos[2],O_CREAT | O_EXCL | O_WRONLY, 0644)==-1){
+            df=open(trozos[2],O_CREAT | O_EXCL, 0644);
+            if(df==-1){
                 perror("Imposible crear");
+            }
+            else{
+                close(df);
             }
         }
     }
     else{
-        //Crear directorio
         if(mkdir(trozos[1],0777)!=0){
             perror("Imposible crear");
         }
@@ -51,25 +55,30 @@ void stats(char *trozos[], int numWords){
     bool largo=false, access=false, enlace=false;
     int i=0;
 
-    for(i=0;i<numWords;i++){
-        if(trozos[1]==NULL){
-            printRoute();
-        }
-        else if(strcmp(trozos[i],"-long")==0){
-            largo=true;
-        }
-        else if(strcmp(trozos[i],"-acc")==0){
-            access=true;
-        }
-        else if(strcmp(trozos[i],"-link")==0){
-            enlace=false;
-        }
-        else{
-            break;
+    if(trozos[1]==NULL){
+        printRoute();
+    }
+    else if(trozos[2]==NULL){
+        printf("hola\n");
+    }
+    else{
+        for(i=1;i<numWords;i++){
+            if(strcmp(trozos[i],"-long")==0){
+                largo=true;
+            }
+            else if(strcmp(trozos[i],"-acc")==0){
+                access=true;
+            }
+            else if(strcmp(trozos[i],"-link")==0){
+                enlace=false;
+            }
+            else{
+                break;
+            }
         }
     }
     
-    i++; //Para saber donde acaban los argumentos. Si hay 1 argumento entonces trozos[2] es fichero e "i" vale 2
+    //i++; //Para saber donde acaban los argumentos. Si hay 1 argumento entonces trozos[2] es fichero e "i" vale 2
     printf("%d %d %d %d\n",i,largo,access,enlace);
     
 }
